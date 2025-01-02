83d05d2a2ebae0fbb144fefc643564bc311b627a
kaitok <kaitokinjo@gmail.com>
2025-01-02 12:13:26 +0900

    return after mounted

--- src/components/layout/ThemeProvider.tsx
+++ src/components/layout/ThemeProvider.tsx
@@ -1,10 +1,19 @@
 "use client";
 import type { ComponentProps } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useState, useEffect } from "react";
 
 export function ThemeProvider({
 	children,
 	...props
 }: ComponentProps<typeof NextThemesProvider>) {
-	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+	const [mounted, setMounted] = useState<boolean>(false);
+	useEffect(() => {
+		setMounted(true);
+		return () => setMounted(false);
+	}, []);
+
+	return (
+		mounted && <NextThemesProvider {...props}>{children}</NextThemesProvider>
+	);
 }
